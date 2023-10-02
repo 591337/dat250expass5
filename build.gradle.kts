6edd583fca3eaa0240a216c16919ece37dd1286d
Martin <591337@stud.hvl.no>
2023-10-02 13:14:27 +0200

    experiment 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,13 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.1.4"
 	id("io.spring.dependency-management") version "1.1.3"
+	kotlin("jvm") version "1.9.20-Beta2"
 }
 
 group = "no.hvl.dat250"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
@@ -18,8 +18,13 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+kotlin {
+	jvmToolchain(17)
 }
---
+++ src/main/java/no/hvl/dat250/expass5/HelloController.java
@@ -0,0 +1,14 @@
+package no.hvl.dat250.expass5;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
